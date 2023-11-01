61b035d390512018767885840201e26b6f2b3215
estka <estka@gmail.com>
2023-10-31 22:10:42 -0300

    Criação e Configuração dos modelos de veículo e consumo

---
+++ ColabList/Models/Consumo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.PortableExecutable;
+
+namespace ColabList.Models
+{
+    [Table("Consumos")]
+    public class Consumo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Descricao { get; set; }
+        [Required]
+        public DateTime Data { get; set;}
+        [Required]
+        public decimal Valor { get; set;}
+        [Required]
+        public TipoCombustivel Tipo {  get; set; }
+        [Required]
+        public int VeiculoId { get; set; } // essa é  a configuração do entityframework onde o netity entende que veiculo é ta entidade veículo e ID é a chave primária
+        public Veiculo Veiculo { get; set; } // criado objeto do tipo veículo para criar a navegação virtual, que é a possibilidade de navegar de uma entidade para outra. Ou seja, quando o entity carregar inf de uma entidade, ele automaticamente "navega", carregando info de outra 
+        public Collection<Consumo> Consumos{ get; set; }
+    
+    }
+
+    public enum TipoCombustivel
+    {
+        Diesel,
+        Etanol,
+        Gasolina
+    }
+}
---
+++ ColabList/Models/Veículo.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ColabList.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        public string Modelo { get; set; }
+        [Required]
+        public string Placa { get; set; }
+        [Required]
+        public int AnoFabricacao { get; set; }
+        [Required]
+        public int AnoModelo { get; set; }
+        [Required]
+        // como a relação com entity é 1:n não é necessário criar uma prop de consumo mas sim a naveção virtual, sendo:
+        public ICollection<Consumo> Consumos { get; set; }
+        // com isso ue falo que a minha class veiculo tem N consumos e que esses consumos estão associados apenas a um veículo
+    }
+}
