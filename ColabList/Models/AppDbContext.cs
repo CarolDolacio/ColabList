211ca35423a9be7b81edcd4dbafbffcdfdd9e551
estka <estka@gmail.com>
2023-10-31 23:48:32 -0300

    feito update-databse

---
+++ ColabList/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ColabList.Models
+{
+    public class AppDbContext : DbContext 
+    {
+        //esse codigo abaixo é uma injeção de dependecia??
+        // elas serão colocadas em Program.cs
+        public AppDbContext(DbContextOptions options) : base(options) // preciso entender isso daqui melhor
+        {
+        }
+
+        //agora vou colocar aqui quais são as tabela que ele vai criar
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Consumo> Consumos { get; set; }
+        //essa é a config necessario para criar a Classe de contextou ou DbContext
+    }
+}
--- ColabList/Program.cs
+++ ColabList/Program.cs
@@ -1,8 +1,19 @@
+using ColabList.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+//aqui vou colcoar config dos bancos de dados que sao serviços via injeção de dependencia
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+
+);
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
